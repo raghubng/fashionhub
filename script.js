fe9ed9d1252b8bd2da984984cc01f9b863d14568
raghubng <114103601+raghubng@users.noreply.github.com>
2025-06-17 23:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$('.hero-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    loop:true,
+});
+
+
+
+$('.testimonial-area').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    loop:true,
+});
+
+$('.slider-category').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    loop:true,
+});
